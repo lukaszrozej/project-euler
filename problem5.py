fc33bb6390ed42db4184457729e2a4f9b34a7c63
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-10-11 21:32:11 +0200

    add time measurement to problem 5

--- problem5.py
+++ problem5.py
@@ -1,5 +1,6 @@
-# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+import time
 
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 
@@ -15,5 +16,10 @@ def lcm_from_1_to(n):
 	for b in range(2,n+1):
 		a = a * b // gcd(a, b)
 	return a
+
+t = time.process_time()
+lcm_from_1_to(20)
+elapsed_time = time.process_time() - t
+print('time: ', elapsed_time)
 
 print( lcm_from_1_to(20) )

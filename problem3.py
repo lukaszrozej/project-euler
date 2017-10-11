fc85b330a0ab5f875a7889952a2d4ab547d74e91
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-10-11 18:14:42 +0200

    add time measurement to problem 3

--- problem3.py
+++ problem3.py
@@ -1,3 +1,5 @@
+import time
+
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
@@ -17,5 +19,11 @@ def find_largest_prime_factor(number):
 		return number
 	else:
 		return max
+
+t = time.process_time()
+find_largest_prime_factor(600851475143)
+elapsed_time = time.process_time() - t
+print('time: ', elapsed_time)
+
 
 print(find_largest_prime_factor(600851475143))

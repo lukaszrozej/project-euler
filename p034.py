e7bcdd10b8e2cd5e13cc5cdb321fc5ee7e0e3564
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-10-21 10:29:57 +0200

    p034: add problem with solution

---
+++ p034.py
@@ -0,0 +1,13 @@
+# https://projecteuler.net/problem=34
+
+factorials = (1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880)
+
+digits_count = 1
+max_number = 1
+while max_number < digits_count * factorials[9]:
+	digits_count += 1
+	max_number *= 10
+
+s = sum( n for n in range(3, max_number) if n == sum( factorials[int(digit)] for digit in str(n) ) )
+
+print(s)

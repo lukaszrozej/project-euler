1c2a3fda350796c33a684c68f70ede4b6662c170
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-10-21 10:33:55 +0200

    p034: cast digits to int with map

--- p034.py
+++ p034.py
@@ -8,6 +8,6 @@ while max_number < digits_count * factorials[9]:
 	digits_count += 1
 	max_number *= 10
 
-s = sum( n for n in range(3, max_number) if n == sum( factorials[int(digit)] for digit in str(n) ) )
+s = sum( n for n in range(3, max_number) if n == sum( factorials[digit] for digit in map(int, str(n)) ) )
 
 print(s)

f0046e53cd81d90a1dfd41d9b23a3b43786faf61
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-10-28 19:45:29 +0200

    p054: finish solution

--- p054.py
+++ p054.py
@@ -1,10 +1,40 @@
 # https://projecteuler.net/problem=54
 
+import numpy as np
+
 with open('p054_poker.txt') as file:
 	games = file.read().split('\n')
 
+values = '23456789TJQKA'
+
 def rank(hand):
-	return hand
+	flush = all(card[1] == hand[0][1] for card in hand)
+
+	hand = sorted(map(lambda card: values.index(card[0]), hand), reverse=True)
+	unique, counts = np.unique(hand, return_counts=True)
+	repetitions = sorted(zip(counts, unique), reverse=True)
+
+	straight = all( d == -1 for d in np.diff(hand) ) and unique.size == 5
+
+	if straight and flush:
+		return [8] + hand
+	if flush:
+		return [5] + hand
+	if straight:
+		return [4] + hand
+	if repetitions[0][0] == 4:
+		return [7, repetitions[0][1]] + hand
+	if repetitions[0][0] == 3:
+		if repetitions[1][0] == 2:
+			return [6, repetitions[0][1], repetitions[1][1]] + hand
+		else:
+			return [3, repetitions[0][1]] + hand
+	if repetitions[0][0] == 2:
+		if repetitions[1][0] == 2:
+			return [2, repetitions[0][1], repetitions[1][1]] + hand
+		else:
+			return [1, repetitions[0][1]] + hand
+	return [0] + hand
 
 counter = 0
 for game in games:

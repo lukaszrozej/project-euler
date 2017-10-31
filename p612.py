ccd8fc598aeb2122bc574e38a4736b8a38292e61
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-10-31 15:28:59 +0100

    p612: remove modulo from loops

--- p612.py
+++ p612.py
@@ -2,6 +2,11 @@
 
 import numpy as np
 
+# N = 3 -> 289665
+# N = 4 -> 39235977
+# N = 5 -> 4528635021
+# N = 6 -> 481858831665
+
 N = 18
 
 combinations = np.zeros((10, 10), dtype=int)
@@ -13,24 +18,18 @@ for n in range(1,10):
 def comb(n,k):
 	return int(combinations[n,k])
 
-# import pdb
-# pdb.set_trace()
-
 sum_over_k = 0
 for k in range(1,10):
 	sum_over_n = 0
 	for n in range(1, N+1):
 		sum_over_j = 0
 		for j in range(0, k+1):
-			# print(k, n, j, comb(k, j), (k-j)**n)
 			if j % 2 == 0:
 				sum_over_j += comb(k, j) * (k-j)**n
 			else:
 				sum_over_j += -comb(k, j) * (k-j)**n
 		sum_over_n += sum_over_j
-	# print(k, sum_over_n)
 	sum_over_k += comb(9, k) * sum_over_n * (10**N - (10-k)**N)
-	sum_over_k %=  1000267129
 
 for k in range(2,11):
 	sum_over_n = 0
@@ -48,12 +47,9 @@ for k in range(2,11):
 		sub = N
 	else:
 		sub = (10-k) * ((10-k)**N - 1) // (10 - k - 1)
-	# print(k, sum_over_n)
 	sum_over_k += comb(9, k-1) * sum_over_n * (10**N - sub -1)
-	sum_over_k %=  1000267129
 
-# print(sum_over_k)
-
-result = ((sum_over_k - 10**N + 1) // 2) % 1000267129
+result = ((sum_over_k - 10**N + 1) // 2)
+result %= 1000267129
 
 print(result)

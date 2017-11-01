7ef75830c23ee5c181b7f5b26a8c770b9aeb5d2c
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-11-01 08:33:29 +0100

    p059: add solution

--- p059.py
+++ p059.py
@@ -1,21 +1,43 @@
 # https://projecteuler.net/problem=59
 
+import numpy as np
+from  more_itertools import unique_everseen
+from  itertools import product
+
 with open('p059_cipher.txt') as file:
 	cipher = list(map(int, file.read().split(',')))
 
 
 def decipher(cipher, key):
 	length = len(key)
-	numbers = ( cipher[n] ^ ord(key[n % length]) for n in range(0, len(cipher)) )
+	numbers = ( cipher[n] ^ key[n % length] for n in range(0, len(cipher)) )
 	characters = map(chr, numbers)
 	return ''.join(characters)
 
-text = 'Hello World'
-key = 'abc'
-l = list( map(ord, text) )
-print(l)
-cipher = decipher(l, key)
+lists = [[],[],[]]
+for n in range(0, len(cipher)):
+	lists[n % 3].append(cipher[n])
 
-l = list( map(ord, cipher) )
+for n in range(0,3):
+	lists[n] = list( unique_everseen( sorted(lists[n], key=lists[n].count, reverse=True) ) )
 
-print(decipher(l, key))
+
+key = [0,0,0]
+
+# for chars in product(' e', repeat=3):
+# 	for n in range(0,3):
+# 		key[n] = ord(chars[n]) ^ lists[n][0]
+# 	print('CHARS: ', chars)
+# 	print(decipher(cipher, key))
+
+# the most common character is ' '
+
+for n in range(0,3):
+	key[n] = ord(' ') ^ lists[n][0]
+
+text = decipher(cipher, key)
+
+s = sum( ord(c) for c in text )
+
+print(text)
+print(s)
